7bc1ddeb5d638ccadf2bca8ebb858d659d00a101
GUIDO.RODLER <GUIDO.RODLER@buonarroti.tn.it>
2022-11-09 16:11:42 +0100

    feat:added method monster

---
+++ src/com/company/Monster.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Monster {
+    String nome;
+    int punti_vita;
+    int danno;
+    //metodi
+    Monster(String nome,int punti_vita){
+        this.nome=nome;
+        this.punti_vita=punti_vita;
+    }
+    public String attacca(){
+
+    }
+    public String subisci(){
+
+    }
+
+}
